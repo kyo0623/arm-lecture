85e65aeafae40d266d8227eb51856c8e99df4c7f
FU JHONG YANG <kyo0623@hotmail.com>
2015-09-30 23:21:34 +0800

    Iterative

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,24 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
+	
+mov r3, #-1	@ previous = -1;
+mov r4, r0	@ i = x(r0);
+mov r5, #0	@ sum = 0;
+mov r0, #1	@ result  = 1;
+	
+.Loop:
+cmp r4, #-1 	@ if (i <= -1);
+ble .Exit	@ Exit
+sub r4, r4, #1	@ i = i - 1;
+add r5, r0, r3	@ sum = result + previous;
+mov r3, r0	@ previous = result;
+mov r0, r5	@ result = sum;
+b .Loop	@ Loop
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
+.Exit:
 	pop {r3, r4, r5, pc}		@EPILOG
-
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
