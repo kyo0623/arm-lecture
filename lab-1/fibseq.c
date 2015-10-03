6b56e019a947504f2a20ce708ce2d6dc2b07f458
FU JHONG YANG <gomak0829@gmail.com>
2015-10-03 17:44:03 +0800

    Tail recursive method

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <time.h>
-extern int fibonacci(int x);
+extern int fibonacci(int x, int y, int z);
 
 int main(int argc, char **argv)
 {
   clock_t  start, finish;
   double time;
   int number =0;
+  int y = 0;
+  int z = 1;
   int result =0;
   FILE *out;
   out = fopen("result.txt","a"); 
   scanf("%d",&number);
   start = clock();
-  result = fibonacci(number);   
+  result = fibonacci(number, y, z);   
   finish = clock();
   time = (double)(finish - start) / CLOCKS_PER_SEC;
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+
   fprintf(out, "%d\t%f\n",number, time);
   fclose(out);
 }
