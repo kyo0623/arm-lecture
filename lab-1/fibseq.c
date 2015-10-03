2547203aa2629a9ec4a73027930146c83477a6d5
FU JHONG YANG <gomak0829@gmail.com>
2015-10-03 17:41:25 +0800

    calculate time and recursive method

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
-
+  clock_t  start, finish;
+  double time;
+  int number =0;
+  int result =0;
+  FILE *out;
+  out = fopen("result.txt","a"); 
   scanf("%d",&number);
+  start = clock();
   result = fibonacci(number);   
+  finish = clock();
+  time = (double)(finish - start) / CLOCKS_PER_SEC;
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  fprintf(out, "%d\t%f\n",number, time);
+  fclose(out);
 }
-
